e1592b2332db9bc320e26356a285de596eb439ee
phil10xs <philipigbobagmail.com>
2020-11-19 09:18:41 +0100

    Here is my continous message for the app. I had completed the clock design and functionality. I would be focusing on the Homepage UI experience.

--- README.md
+++ README.md
@@ -3,14 +3,6 @@
 A new Flutter application.
 
 ## Getting Started
+This is the beta version of the TimeZ project app that displays time zones globally.
+Works as a great travel companion. The CustomPainter Widget is used in this beta to paint a clockview that would update it's state based on flutter Time.periodic Widget property
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
--- lib/clock_view.dart
+++ lib/clock_view.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class ClockView extends StatefulWidget {
@@ -6,25 +9,86 @@ class ClockView extends StatefulWidget {
 }
 
 class _ClockViewState extends State<ClockView> {
+  @override  void initState() {
+   Timer.periodic(Duration(seconds:1),(timer){
+  setState(() {
+  });
+} );
+    super.initState();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 300,
         height: 300,
+        child: Transform.rotate(angle: -pi/2,
         child: CustomPaint(
           painter: ClockPainter(),
          ),
-    );
+    ));
    }
 }
 
 
 class ClockPainter extends CustomPainter
 {
-@override
-  void paint(Canvas canvas, Size size){
-}
+  var dateTime = DateTime.now();
 
+@override
+  void paint(Canvas canvas, Size size) {
+  var centerX = size.width / 2;
+  var centerY = size.height / 2;
+  var center = Offset(centerX, centerY);
+  var radius = min(centerX, centerY);
+
+  var fillBrush = Paint()
+  ..color = Color(0xFF444974);
+
+  var outlineBrush = Paint()
+    ..color = Color(0xFFEAECFF)
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 16;
+
+  var centerFillBrush = Paint()
+    ..color = Color(0xFFEAECFF);
+
+  var secHandBrush = Paint()
+    ..color = Colors.orange[300]
+    ..style = PaintingStyle.stroke
+    ..strokeCap = StrokeCap.round
+    ..strokeWidth = 8;
+
+  var minHandBrush = Paint()
+    ..shader = RadialGradient(colors: [Color(0xFF748EF6), Color(0xFF77DDFF)]).createShader(Rect.fromCircle(center: center, radius: radius))
+    ..style = PaintingStyle.stroke
+    ..strokeCap = StrokeCap.round
+    ..strokeWidth = 16;
+
+  var hourHandBrush = Paint()
+    ..shader = RadialGradient(colors: [Color(0xFEA74AB), Color(0xFFC279FB)]).createShader(Rect.fromCircle(center: center, radius: radius))
+    ..style = PaintingStyle.stroke
+    ..strokeCap = StrokeCap.round
+    ..strokeWidth = 16;
+
+
+  canvas.drawCircle(center, radius -40, fillBrush);
+  canvas.drawCircle(center, radius -40, outlineBrush);
+
+  var hourHandX = centerX + 60 * cos((dateTime.hour * 30 + dateTime.minute * 0.5) * pi/180);
+  var hourHandY = centerX + 60 * sin((dateTime.hour * 30 + dateTime.minute * 0.5) * pi/180);
+  canvas.drawLine(center, Offset(hourHandX,hourHandY), hourHandBrush);
+
+  var minHandX =  centerX + 80 * cos(dateTime.minute * 6 * pi/180);
+  var minHandY = centerX +  80 * sin (dateTime.minute * 6 * pi/180);
+  canvas.drawLine(center, Offset(minHandX,minHandY), minHandBrush);
+
+  var secHandX = centerX +  80 * cos(dateTime.second * 6 * pi/180);
+  var secHandY = centerX +  80 * sin(dateTime.second * 6 * pi/180);
+  canvas.drawLine(center, Offset(secHandX,secHandY), secHandBrush);
+
+  canvas.drawCircle(center, 16, centerFillBrush);
+}
 @override
   bool shouldRepaint(CustomPainter oldDelegate){
   return true;
